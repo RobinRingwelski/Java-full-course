8295c327edaf1f8c46f8b046b95f8c8147a2b0aa
Robin Ringwelski <94263613+Thohard-Code@users.noreply.github.com>
2025-01-13 18:58:28 +0100

    Loops and logic operator done

--- JavaCourse/src/Main.java
+++ JavaCourse/src/Main.java
@@ -6,43 +6,14 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        double num1;
-        double num2;
-        char operator;
-        double result = 0;
-        boolean validOperator = true;
+        int number = 0;
 
-        System.out.print("Enter the first number: ");
-        num1 = scanner.nextDouble();
+        do{
+            System.out.print("Enter a number between 1 - 10: ");
+            number = scanner.nextInt();
+        } while(number < 1 || number > 10);
 
-        System.out.print("Enter an operator (+, -, *, /, ^): ");
-        operator = scanner.next().charAt(0);
-
-        System.out.print("Enter the second number: ");
-        num2 = scanner.nextDouble();
-
-        switch (operator) {
-            case '+' -> result = num1 + num2;
-            case '-' -> result = num1 - num2;
-            case '*' -> result = num1 * num2;
-            case '/' -> {
-                if (num2 == 0) {
-                    System.out.println("Dont divide by zero!");
-                    validOperator = false;
-                } else {
-                    result = num1 / num2;
-                }
-            }
-            case '^' -> result = Math.pow(num1, num2);
-            default -> {
-                System.out.println("Invalid operator!");
-                validOperator = false;
-            }
-        }
-
-        if (validOperator) {
-            System.out.println(result);
-        }
+        System.out.println("You picked " + number);
 
         scanner.close();
     }
