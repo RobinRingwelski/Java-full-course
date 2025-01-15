daf9f1711eeb38b373f8a7b5359b69a8b8f89c03
Robin Ringwelski <94263613+Thohard-Code@users.noreply.github.com>
2025-01-15 19:13:35 +0100

    Forloops
    Simple forloop with scanner input

--- JavaCourse/src/Main.java
+++ JavaCourse/src/Main.java
@@ -2,18 +2,20 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        // for loop = execute a code a CERTAIN amount of time
 
         Scanner scanner = new Scanner(System.in);
 
-        int number = 0;
+        System.out.print("How many seconds for the countdown?: ");
+        int start = scanner.nextInt();
 
-        do{
-            System.out.print("Enter a number between 1 - 10: ");
-            number = scanner.nextInt();
-        } while(number < 1 || number > 10);
-
-        System.out.println("You picked " + number);
+        for(int i = start; i > 0; i--){
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+        System.out.println("Happy new year!");
 
         scanner.close();
     }
