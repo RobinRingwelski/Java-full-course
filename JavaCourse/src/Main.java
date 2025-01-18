36b774401e7408e21537d9207fbf1fddd211d064
Robin Ringwelski <94263613+RobinRingwelski@users.noreply.github.com>
2025-01-18 11:50:08 +0100

    Overloaded methods()

--- JavaCourse/src/Main.java
+++ JavaCourse/src/Main.java
@@ -3,48 +3,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // method = a block of reusable code that is executed when called ()
+        // Overloaded methods = methods that share the same name, but different parameters.
 
+        String pizza = bakePizza("flat bread", "mozzarella", "pepperoni");
 
-        String name = "Robin";
-        int age = 20;
-
-        happyBirthday(name, age);
-
-        System.out.println(square(3));
-
-        System.out.println(cube(3));
-
-        String fullName = getFullName("Spongebob", "Squarepants");
-        System.out.println(fullName);
-
-        if(ageCheck(age)){
-            System.out.println("You may sign up!");
-        }
-        else{
-            System.out.println("You must be 18+ to sign up!");
-        }
-
+        System.out.println(pizza);
     }
 
-    static void happyBirthday(String name, int age){
-        System.out.println("Happy Birthday " + name + " You are " + age + " years old!");
+    static String bakePizza(String bread){
+        return bread + " pizza";
     }
 
-    static double square(double number){
-        return number * number;
+    static String bakePizza(String bread, String cheese){
+        return cheese + " " + bread + " pizza";
     }
 
-    static double cube(double number){
-        return number * number * number;
-    }
-
-    static String getFullName(String name, String lastName){
-        return name + " " + lastName;
-    }
-
-    static boolean ageCheck(int age){
-        return age >= 18;
+    static String bakePizza(String bread, String cheese, String topping){
+        return topping + " " + cheese + " " + bread + " pizza";
     }
 
 }
