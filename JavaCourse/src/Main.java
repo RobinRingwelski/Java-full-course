b16aaed96b55a38716bdb1c0a6ad2449113e0918
Robin Ringwelski <94263613+RobinRingwelski@users.noreply.github.com>
2025-01-26 17:11:07 +0100

    Objects (OOP)

--- JavaCourse/src/Main.java
+++ JavaCourse/src/Main.java
@@ -3,28 +3,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // 2D array = An array where each element is an array
-        // Useful for storing matrix of data
+        // Object = An entity that holds data (attributes)
+        //          and can perform actions (methods)
+        //          It is a reference data type
 
-        // 1D arrays
-        String[] fruits = {"apple", "orange", "banana"};
-        String[] vegetables = {"potato", "onion", "carrot"};
-        String[] meats = {"chicken", "pork", "beef", "fish"};
+        Car car = new Car();
 
-        // 2D array or grid/matrix (dosen't need to be the array name could also just be the array itself)
-        String[][] groceries = {fruits, vegetables, meats};
-
-        // changing something inside the 2D array you should use both indexes of the array and then the item
-        groceries[0][0] = "pineapple";
-        groceries[1][0] = "celery";
-
-        for(String[] foods : groceries){
-            for(String food : foods){
-                System.out.print(food + " ");
-            }
-            System.out.println();
-        }
-
+        System.out.println(car.isRunning);
+        car.start();
+        System.out.println(car.isRunning);
+        car.stop();
+        System.out.println(car.isRunning);
+        car.start();
+        car.brake();
+        car.drive();
     }
 
 }
