5e19012c86a7c1da4824acc819f009ee15a7f88d
Robin Ringwelski <94263613+RobinRingwelski@users.noreply.github.com>
2025-01-27 19:41:06 +0100

    Static keyword

--- JavaCourse/src/Main.java
+++ JavaCourse/src/Main.java
@@ -3,24 +3,17 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // making Car objects
-        Car car1 = new Car("Peugeot", "Yellow");
-        Car car2 = new Car("Corvette", "Red");
-        Car car3 = new Car("Charger", "Green");
+        // Static = Makes a variable or method belong to the class
+        //          rather than to any specific object
+        //          commonly used for utility methods or shared resources.
 
-        // making object array
-        Car[] cars = {car1, car2, car3};
+        Friend friend1 = new Friend("Spongebob");
+        Friend friend2 = new Friend("Patrick");
+        Friend friend3 = new Friend("Sandy");
+        Friend friend4 = new Friend("Squidward");
+        Friend friend5 = new Friend("Gary");
 
-        // Could also be done like this
-/*        Car[] cars = {new Car("Peugeot", "Yellow"),
-                        new Car("Corvette", "Red"),
-                        new Car("Charger", "Green")};
-*/
-
-        // for every Car object in our car array do method
-        for (Car car : cars) {
-            car.drive();
-        }
+       Friend.showFriends();
 
     }
 
