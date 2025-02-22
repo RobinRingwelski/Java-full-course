a6713443e9e074c2dde0572305d03d128cc2e386
Robin Ringwelski <94263613+RobinRingwelski@users.noreply.github.com>
2025-02-22 14:08:21 +0100

    DATES &  TIMES in Java

--- JavaCourse/src/Main.java
+++ JavaCourse/src/Main.java
@@ -1,29 +1,19 @@
-import java.io.*;
-import java.io.FileNotFoundException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
 
     public static void main(String[] args)  {
 
-        // How to read a file using Java (3 popular options)
+        // How to work with DATES & TIMES using Java
+        // (LocalDate, LocalTime, LocalDateTime, UTC timestamp)
 
-        // BufferedReader + FileReader: Best for reading text files line-by-line
-        // FileInputSteam: Best for binary files (e.g, images, audio files)
-        // RandomAccessFile: Best for read/write specific portions of a large file
+        // Custom format
+        LocalDateTime dateTime = LocalDateTime.now();
 
-        String filePath = "C:\\Users\\Ringw\\Desktop\\test.txt";
-        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))){
-            String line;
-            while((line = reader.readLine()) != null){
-                System.out.println(line);
-            }
-
-        } catch (FileNotFoundException e){
-            System.out.println("File could not be found!");
-        }
-        catch (IOException e){
-            System.out.println("Something went wrong!");
-        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String newDateTime = dateTime.format(formatter);
+        System.out.println(newDateTime);
 
     }
 }
