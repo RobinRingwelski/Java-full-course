625fe44b957b04717ffbfcf0b2b8a6a13ca3f5f9
Robin Ringwelski <94263613+RobinRingwelski@users.noreply.github.com>
2025-02-25 17:31:56 +0100

    COUNTDOWN mini project in (JAVA)

--- JavaCourse/src/Main.java
+++ JavaCourse/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -5,24 +6,24 @@ public class Main {
 
     public static void main(String[] args)  {
 
-        // Timer = Class that schedules tasks at specific times or periodically
-        //         Useful for: sending notifications, scheduled updates, repetitive actions.
+        // Java COUNTDOWN TIMER PROGRAM
 
-        // TimerTask = Represents the task that will be executed by the Timer
-        //             You will extend the TimerTask class to define your task
-        //             Create a subclass of TimerTask and @Override run()
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number of seconds for countdown: ");
+        int response = scanner.nextInt();
 
         Timer timer = new Timer();
         TimerTask task = new TimerTask() {
 
-            int count = 3;
+            int count = response;
 
             @Override
             public void run() {
-                System.out.println("Hello!");
+                System.out.println(count);
                 count--;
                 if(count <= 0){
-                    System.out.println("Task completed");
+                    System.out.println("Happy New Year!");
                     timer.cancel();
                 }
             }
