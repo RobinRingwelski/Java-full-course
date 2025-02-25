5abc0549cd07cc866c59b3bfbf535a460ec7f2b1
Robin Ringwelski <94263613+RobinRingwelski@users.noreply.github.com>
2025-02-25 17:51:42 +0100

    Generics in (Java)

--- JavaCourse/src/Main.java
+++ JavaCourse/src/Main.java
@@ -1,35 +1,19 @@
-import java.util.Scanner;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class Main {
 
     public static void main(String[] args)  {
 
-        // Java COUNTDOWN TIMER PROGRAM
+        // Generics = A concept where you can write a class, interface, or method
+        //            that is compatible with different data types.
+        //            <T> type parameter (placeholder that gets replaced with a real type)
+        //            <String> type argument (specifies the type)
 
-        Scanner scanner = new Scanner(System.in);
+        Box<Integer> box = new Box<>();
+        Product<String, Double> product1 = new Product<>("Banana", 1.25);
+        Product<String, Integer> product2 = new Product<>("MovieTicket", 15);
 
-        System.out.println("Enter number of seconds for countdown: ");
-        int response = scanner.nextInt();
-
-        Timer timer = new Timer();
-        TimerTask task = new TimerTask() {
-
-            int count = response;
-
-            @Override
-            public void run() {
-                System.out.println(count);
-                count--;
-                if(count <= 0){
-                    System.out.println("Happy New Year!");
-                    timer.cancel();
-                }
-            }
-        };
-
-        timer.schedule(task, 0, 1000);
+        System.out.println("The first product is a " + product1.getItem() + " and it costs: " + product1.getPrice());
+        System.out.println("The second product is a " + product2.getItem() + " and it costs: " + product2.getPrice());
 
     }
 }
